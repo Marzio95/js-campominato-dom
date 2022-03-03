2055858e85c2352d470573625a8bc1f9cdcc92a2
Marzio95 <marziodellarocca14@gmail.com>
2022-03-03 16:29:31 +0100

    cambio il ciclo for in un ciclo while ed aggiungo un tasto controllo su ogni box

--- js/script.js
+++ js/script.js
@@ -3,59 +3,100 @@ const containerGame = document.querySelector('.container_game');
 
 const btnPlay = document.getElementById('button_play');
 let difficultInput = document.getElementById('difficult');
-let arrayBomb = [];
-console.log(arrayBomb);
 
 btnPlay.addEventListener('click', myGame);
+let arrayBomb = [];
 
 function myGame() {
-    
+
     containerGame.innerHTML = '';
-    
+
+
     if (difficultInput.value == 'Hard') {
-        for (let index = 1; index <= 16; index++) {
+        arrayBomb = [];
+        console.log(arrayBomb);
+        while (arrayBomb.length < 16) {
             let bomb = Math.floor(Math.random() * 49) + 1;
-            console.log(bomb)
-            if (arrayBomb.includes(bomb)){
-                !arrayBomb.push(bomb)
-            } else {
-                arrayBomb.push(bomb)
+            while (arrayBomb.includes(bomb)) {
+                bomb = Math.floor(Math.random() * 49) + 1;
             }
+            arrayBomb.push(bomb);
         }
+        
         difficultInput.value = '';
         for (let i = 1; i <= 49; i++) {
             let box = document.createElement('div');
             box.classList.add('quadrato');
             box.innerHTML = i;
             containerGame.append(box);
-            box.addEventListener('click', colorChange);
+            box.addEventListener('click', controllo);
+
+            function controllo() {
+                if (arrayBomb.includes(i)) {
+                    this.classList.add('colore_bad');
+                } else {
+                    this.classList.add('colore_good');
+                }
+            }
         }
 
     } else if (difficultInput.value == 'Medium') {
+        arrayBomb = [];
+        console.log(arrayBomb);
+        while (arrayBomb.length < 16) {
+            let bomb = Math.floor(Math.random() * 81) + 1;
+            while (arrayBomb.includes(bomb)) {
+                bomb = Math.floor(Math.random() * 81) + 1;
+            }
+            arrayBomb.push(bomb);
+        }
+
         difficultInput.value = '';
         for (let i = 1; i <= 81; i++) {
             let box = document.createElement('div');
             box.classList.add('quadrato2');
             box.innerHTML = i;
             containerGame.append(box);
-            box.addEventListener('click', colorChange);
+            box.addEventListener('click', controllo);
+
+            function controllo() {
+                if (arrayBomb.includes(i)) {
+                    this.classList.add('colore_bad');
+                } else {
+                    this.classList.add('colore_good');
+                }
+            }
         }
     } else if (difficultInput.value == 'Easy') {
+        arrayBomb = [];
+        console.log(arrayBomb);
+        while (arrayBomb.length < 16) {
+            let bomb = Math.floor(Math.random() * 100) + 1;
+            while (arrayBomb.includes(bomb)) {
+                bomb = Math.floor(Math.random() * 100) + 1;
+            }
+            arrayBomb.push(bomb);
+        }
+
         difficultInput.value = '';
         for (let i = 1; i <= 100; i++) {
             let box = document.createElement('div');
             box.classList.add('quadrato3');
             box.innerHTML = i;
             containerGame.append(box);
-            box.addEventListener('click', colorChange);
+            box.addEventListener('click', controllo);
+
+            function controllo() {
+                if (arrayBomb.includes(i)) {
+                    this.classList.add('colore_bad');
+                } else {
+                    this.classList.add('colore_good');
+                }
+            }
         }
     } else {
 
     }
 
 
-}
-
-function colorChange(){
-    this.classList.add('colore_new');
 }
